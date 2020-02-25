ec561ae02a7667eec60836ad93178b71781269e3
DanielSzpunar <danielszpunar@gmail.com>
2020-02-24 23:54:33 -0500

    part1b javscript map and filter

--- part1/src/index.js
+++ part1/src/index.js
@@ -44,20 +44,36 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    // const course = 'Half Stack application development'
+    // const part1 = 'Fundamentals of React'
+    // const exercises1 = 10
+    // const part2 = 'Using props to pass data'
+    // const exercises2 = 7
+    // const part3 = 'State of a component'
+    // const exercises3 = 14
+    const course = 'Half Stack Application Development'
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a Component',
+            exercises: 14
+        }
+    ]
+
     return (
         <>
             <Header course={course}/>
-            <Content part={part1} exercise={exercises1} />
-            <Content part={part2} exercise={exercises2}/>
-            <Content part={part3} exercise={exercises3}/>
-            <Total total={exercises1 + exercises2 + exercises3} />
+            <Content part={parts[0].name} exercise={parts[0].exercises} />
+            <Content part={parts[1].name} exercise={parts[1].exercises}/>
+            <Content part={parts[2].name} exercise={parts[2].exercises}/>
+            <Total total={parts[0].exercises + parts[0].exercises + parts[2].exercises} />
         </>
     )
 }
